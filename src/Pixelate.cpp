d78c0f5c7e57d58d34160edc06bf196c27ecf90b
Charles Mailly <charles.mailly@free.fr>
2016-04-15 15:50:20 +0200

    Pixel_Threshold Everywhere

--- src/Pixelate.cpp
+++ src/Pixelate.cpp
@@ -8,9 +8,7 @@ Pixelate::Pixelate()
 void Pixelate::apply(sf::RenderTexture const& input, sf::RenderTarget& output)
 {
     sf::Shader& shader = NWorld::getResources().getShader("pixelate");
-    float x = NWorld::getPointerPositionScreen().x / NWorld::getWindow().getSize().x;
-    float y = NWorld::getPointerPositionScreen().y / NWorld::getWindow().getSize().y;
     shader.setParameter("texture",input.getTexture());
-    shader.setParameter("pixel_threshold", (x + y) / 30);
+    shader.setParameter("pixel_threshold", 0.004f);
     applyShader(shader,output);
 }
